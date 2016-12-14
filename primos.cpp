fe4e1c1eb74d2f5102d8fb7355261068d2be720b
cesarrison <cesarrison@gmail.com>
2016-12-14 15:29:23 -0300

    Update primos.cpp

--- primos.cpp
+++ primos.cpp
@@ -1,30 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-//comprueba si un número es primo
-
+int main(void)
 {
-
-int num1,i,a;
-
-a=0; //Esta variable se utiliza para contar los divisores del número introducido, se inicializa a 0
-
-printf("Introduce un numero: ");
-scanf("%d",&num1);
-
-for(i=1;i<=num1;i++)
-{
-    if(num1%i==0) // si num1 módulo de i es 0, incrementamos a en 1.
-    a++;
-}
-
-
-if(a==2); //Si al final del bucle, a vale 2, el número es primo, porque tiene solamente 2 divisores.
-{
-    printf("El número es primo");
-}
-else
-{
-    printf("El número no es primo"); 
-}
-
-
+    int n, c, divisores; 
+    
+    do{
+        printf("Introduzca el numero a determinar si es o no primo:\n");
+        scanf("%d",&n);
+    }while(!(n>=0)); /*He incluido un bucle do-while, para que el programa comience cuando le den un numero estrictamente positivo */
+    
+    divisores = 0; /*inicializo el contador divisores a 0 */
+    
+    for(c=2; c<=n/2; c++) /*Divido hasta n/2, porque superior a n/2 no hay divisores (digo yo...) */
+    if (n%c == 0) divisores++; /*Si el resto es cero, aumento en uno el contador divisores*/
+    
+    if (divisores >= 1) printf ("%d no es primo\n", n); 
+    else printf ("%d es primo\n",n);
+    
+    system("pause");
+    return(0);
 }
